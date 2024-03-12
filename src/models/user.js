109cce768015879e2a81143bc2181f56219be540
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2024-03-12 20:56:39 +0200

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const validatePhoneNumber = require("validate-phone-number-node-js");
 
 const userSchema = new mongoose.Schema({
   firstname: {
