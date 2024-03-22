1854a1ee83bf2840c27da3299ffd207689ee5810
Ahmed-Mohamed-Hassen-Mohamed <93393919+Ahmed-Mohamed-Hassen-Mohamed@users.noreply.github.com>
2024-03-22 14:25:24 +0200

    Update startExam.js

--- src/routers/startExam.js
+++ src/routers/startExam.js
@@ -6,6 +6,7 @@ const exam = require("../controllers/startExam");
 
 router.post("/startExam", userAuth, exam.startExam);
 router.get("/myExams", userAuth, exam.getMyExams);
+router.get("/examResults/:id", userAuth, instructorAuth, exam.getExamResults);
 router.get("/myExams/:id", userAuth, exam.getExamById);
 
 module.exports = router;
